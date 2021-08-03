74d0a16a053bad38ee55a41b0f55b0c844f9dfae
Thammasorn Yimsawat <thammasorn.y@ku.th>
2021-08-03 14:37:32 +0700

    edit login feature

--- src/store/Shop.js
+++ src/store/Shop.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import AuthService from '@/services/AuthService'
 
 let end_point = process.env.SHOP_ENDPOINT || 'http://localhost:1337'
+
 Vue.use(Vuex)
+
+let auth_key = "auth-shop"
+let auth = JSON.parse(localStorage.getItem(auth_key))
+const initialStateUser = {
+  user: auth ? auth.user : "",
+  jwt: auth ? auth.jwt : "",
+  isAuthen :auth ? true : false
+}
 
 export default new Vuex.Store({
   state: {
-      allUser: [],
-      currentUser:'',
+      currentUser: initialStateUser,
       product: [],
       reward: [],
       point: [],
@@ -18,26 +27,20 @@ export default new Vuex.Store({
       getCurrentUser: (state) => state.currentUser
   },
   mutations: {
-    fetchUser(state, { res }){
-        state.allUser = res.data
-      },
     fetchProduct(state, { res }){
         state.product = res.data
       },
     fetchReward(state, { res }){
         state.reward = res.data
       },
-    setCurrentUser(state, res){
-        state.currentUser = res
+    setCurrentUser(state, user , jwt){
+        state.currentUser.user = user,
+        state.currentUser.jwt = jwt,
+        state.currentUser.isAuthen = true
     },
+    
   },
   actions: {
-    async fetchAlluser({commit}){
-      let res = await axios.get(end_point + "/userdata")
-      // console.log(res)
-      commit('fetchUser',{res})
-
-    },
     async fetchAllproduct({commit}){
       let res = await axios.get(end_point + "/products")
       // console.log(res)
@@ -49,8 +52,15 @@ export default new Vuex.Store({
       commit('fetchReward',{res})
     },
     setCurrentUser({commit},payload){
-      // console.log(payload);
+      console.log(payload);
       commit('setCurrentUser',payload)
+    },
+    async login({commit},{username, password}){
+      let res = await AuthService.login({username, password})
+      if(res.success){
+        commit('setCurrentUser', res.user, res.jwt)
+      }
+      return res
     }
   },
   modules: {
