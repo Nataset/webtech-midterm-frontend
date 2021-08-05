f6078b9df5f7e3a59fcdb6e7b7ce3187968c30ca
kt_boom <kritsada.tan@hotmail.com>
2021-08-05 21:00:05 +0700

    add style for topup

--- src/views/Topup.vue
+++ src/views/Topup.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="topup-container">
-    <div v-if="isAuthen()">
-      <h2>Your Total Value : {{ currentUser.money }}</h2>
-      <h3>Pumping Your Money</h3>
-      <form @submit.prevent="addMoney">
-        <div>
-          <label>Amount:</label>
-          <input v-model="form.amount" type="number" />
-          <button type="submit">Add</button>
-        </div>
-      </form>
+  <div >
+    <div class="topup-container" v-if="isAuthen()">
+      <div class="card">
+          <h2 class="card-title">Your Wallet Balance : </h2>
+          <h3 class="card-text">{{ currentUser.money }} Bath</h3>
+      </div>
+      <div class="form-warp">
+        <h4>Pumping Your Money</h4>
+        <form @submit.prevent="addMoney">
+            <div class="amount">
+                <label>Amount:</label>
+                <input v-model="form.amount" type="number" placeholder="0" />
+            </div>
+            <button type="submit">Add</button>
+        </form>
+      </div>
     </div>
     <div v-if="!isAuthen()">
       <h2>
@@ -34,7 +39,7 @@ import ShopStore from "@/store/Shop";
 export default {
   data() {
     return {
-      form: { amount: 0 },
+      form: { amount: "" },
       allUser: [],
       currentUser: [],
       currentMoney: 0,
@@ -85,11 +90,107 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .topup-container {
-  text-align: center;
-  h2,
-  h3 {
-    margin: 0.5em;
-  }
+  h2, h3 {
+        margin: .5em;
+    }
+    .card {
+        width: 50%;
+        max-width: 500px;
+        margin: 1em auto;
+        height: 200px;
+        position: relative;
+        background-color: hsl(358, 84%, 55%);
+        color: white;
+        
+        &::before, &::after {
+            content: '';
+            position: absolute;
+            border: 1px solid white;
+            bottom: .8rem;
+            right: .8rem;
+        }
+        &::before {
+            top: 1rem;
+        }
+        &::after {
+            left: 1rem;
+        }
+    }
+    .card-title {
+        font-size: 2.5rem;
+        line-height: 1;
+        margin-bottom: .5em;
+        position: relative;
+        z-index: 1;
+        color: white;
+        &::before {
+            content: '';
+            position: absolute;
+            background: black;
+            width: calc(100% + 4.5rem);
+            top: -.1em;
+            bottom: -.15em;
+            left: -5rem;
+            z-index: -1;
+            box-shadow: inset -15px 0 10px -10px hsla(358, 84%, 55%, 0.6);
+        }
+        &::after {
+            content: '';
+            position: absolute;
+            top: -.1em;
+            bottom: -.15em;
+            left: calc(100% + 1.25rem);
+            width: 3rem;
+            background: black;
+            box-shadow: inset 12px 0 10px -10px hsla(358, 84%, 55%, 0.6);
+        }
+        
+    }
+    .card-text {
+        text-align: right;
+        padding: 1rem;
+    }
+    .form-warp {
+        text-align: center;
+        background: white;
+        width: 50%;
+        max-width: 500px;
+        margin: 2em auto;
+        border-radius: 4px;
+        padding: 1em;
+        h4 {
+            margin-bottom: 1em;
+        }
+        .amount {
+            margin: 1em;
+            display: flex;
+            justify-content: space-evenly;
+            label {
+                font-size: 1.5em
+            }
+            input {
+                text-align: right;
+            }
+        }
+        button {
+            width: 80px;
+            height: 45px;
+            border-radius: 6px;
+            border: 2px solid black;
+            margin-top: 1em;
+            background-color: hsl(358, 84%, 55%);
+            font-weight: 700;
+            color: white;
+            &:hover {
+                background-color: hsl(118, 72%, 35%);
+                color: white;
+                transform: scale(1.2);
+                transition: ease-out .3s;
+            }
+        }
+    }
 }
-</style>>
+    
+</style>
